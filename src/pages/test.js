f16bb920563b01a8c6e1c829e06de2749b6c0e41
Mokafela <mkd0098@gmail.com>
2021-06-26 15:04:43 +0430

    mobile and desktop separated

---
+++ src/pages/test.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+const test =()=>{
+    return(
+        <main>
+            <h1>porsche panorama </h1>
+        </main>
+    )
+}
+export default test
--- src/svg/svgs.js
+++ src/svg/svgs.js
@@ -2395,5 +2395,6 @@ export const centerOrangeDots =
         </g>
     </svg>
 
-
+export const arrowUp =
+    <svg width="24" height="12" xmlns="http://www.w3.org/2000/svg" fillRule="evenodd" clipRule="evenodd"><path d="M11 2.206l-6.235 7.528-.765-.645 7.521-9 7.479 9-.764.646-6.236-7.53v21.884h-1v-21.883z"/></svg>
 
